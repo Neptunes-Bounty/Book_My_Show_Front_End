15687274345387098d94094988ac8a3f562e8a87
Neptune's Bounty <45900584+Neptunes-Bounty@users.noreply.github.com>
2025-07-13 12:28:57 +0000

    great

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -1,5 +1,5 @@
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/CardGroup";
+import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import { Link } from "react-router-dom";
 import React from "react";
